c70e987d5375efcf1efed38f9d71f64424c3d5e5
SpikeSpiegel <frthery@gmail.com>
2014-11-21 16:58:27 +0100

    ADD scripts for bluetooth autoconnect

---
+++ scripts/btDaemon.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+
+BT1='65:03:36:63:04:07'
+#BT2='48:72:50:EA:49:81'
+
+BT1_STATUS='unknown'
+BT2_STATUS='unknown'
+
+#initialize
+VERBOSE=0
+NO_LOG=0
+COUNT_LINES_LOGGER=0
+MAX_LINES_LOGGER=100
+DEFAULT_DELAI=10
+DELAI=$DEFAULT_DELAI
+LOGGER_FILE=/var/log/btDaemonLogger.log
+if [ "$1" = '-v' ]; then VERBOSE=1; fi
+
+XD_NAME=xboxdrv
+XD_BIN=/usr/bin/$XD_NAME
+XD_CFG1=/home/pi/joysticks/j0.cfg
+XD_CFG2=/home/pi/joysticks/j1.cfg
+XD_ARGS1="--config $XD_CFG1"#"-D -d --deadzone 4000 --dbus disabled --detach"
+XD_ARGS2="--config $XD_CFG2"#"-D -d --deadzone 4000 --dbus disabled --detach"
+#END initialize
+
+#functions
+logger() {
+   if [ $NO_LOG -eq 1 ]; then
+      #no logger
+      return
+   fi
+
+   COUNT_LINES_LOGGER=$(($COUNT_LINES_LOGGER + 1))
+
+   now=$(date +"%m-%d-%y %r")
+   if [ $VERBOSE -eq 1 ]; then
+      echo "[$now]" $1
+   fi
+
+   if [[ $2 -eq 1 ]]; then
+      #RESET Logger File
+      echo "[$now]" $1 > $LOGGER_FILE
+   else
+      echo "[$now]" $1 >> $LOGGER_FILE
+   fi
+}
+
+xd_init() {
+   bt=$1
+   index=$2
+   
+   if [ -x "$XD_BIN" ]; then
+      logger "$XD_BIN is not installed: sudo apt-get install xboxdrv"
+      return
+   fi
+   
+   XD_PID=$(ps -eo pid,command | grep "/bin/bash $XD_BIN" | grep -v grep | awk '{print $1}')
+   if [ ! -z $XD_PID ]; then
+      logger "$XD_BIN is already started [$XD_PID] for bluetooth device $index [$bt]"
+      return
+   fi
+
+   #start xboxdrv with joystick configuration
+   $XD_BIN $XD_ARGS1&
+   RESXD=$?
+   if [ $RESXD = 0 ]; then
+      logger "[OK] $XD_BIN is intialized for bluetooth device $index [$bt]"
+   else
+      logger "[KO] $XD_BIN is not intialized for bluetooth device $index [$bt]"
+   fi
+}
+
+connect() {
+   bt=$1
+   index=$2
+   btStatus='notconnected'
+
+   tryPing=1
+   tryCon=1
+   maxRetry=1
+   #priority to gamepad 1
+   if [ $index -eq 1 ]; then maxRetry=10; fi
+
+   logger "try connecting bluetooth device $index [$bt] maxRetry=$maxRetry..."
+
+   #10 attempts for connecting device
+   while [ $tryPing -le $maxRetry ]; do
+      sudo l2ping $bt -c 3 > /dev/null 2>&1
+      RESPING=$?
+      if [ $RESPING = 0 ]; then
+         logger "[OK][$RESPING] ping bluetooth device $index [$bt]"
+
+         tryCon=1
+         while [ $tryCon -le $maxRetry ]; do
+            sudo bluez-test-input connect $bt &> /dev/null
+            RESCON=$?
+            if [ $RESCON = 0 ]; then
+               logger "[OK][$RESCON] bluetooth device $index [$bt] connected"
+               btStatus='connected'
+               #xd_init $bt $index
+            else
+               logger "[KO][$RESCON][$tryCon] bluetooth device $index [$bt] connected"
+               btStatus='notconnected'
+            fi
+
+            if [ $btStatus = 'connected' ]; then
+               break
+            else
+               tryCon=$(($tryCon + 1))
+            fi
+         done
+      else
+         logger "[KO][$RESPING][$tryPing] ping bluetooth device $index [$bt]"
+         btStatus='noping'
+      fi
+
+      if [ $btStatus = 'connected' ]; then
+         break
+      else
+         tryPing=$(($tryPing + 1))
+      fi
+   done
+
+   if [ $index -eq 1 ]; then
+      BT1_STATUS=$btStatus
+   fi
+   if [ $index -eq 2 ]; then
+      BT2_STATUS=$btStatus
+   fi
+
+   #return btStatus
+}
+#END functions
+
+#MAIN
+#clear
+logger "[START] bluetooth devices monitoring..." 1
+
+while [ 1 ]; do
+   #COUNT_LINES_LOGGER=$(wc -l $LOGGER_FILE | cut -f1 -d" ")
+   if [ $COUNT_LINES_LOGGER -gt $MAX_LINES_LOGGER ]; then
+      #RESET Logger
+      COUNT_LINES_LOGGER=0
+      logger "[FLUSH]bluetooth devices check connections [delai=$DELAI]..." 1
+   else
+      logger "bluetooth devices check connections [delai=$DELAI]..."
+   fi
+
+   hcitool con|grep -v "^Connections:" > /tmp/btcheck
+
+   if [ ! -z "$BT1" ]; then
+      if grep -q $BT1 /tmp/btcheck; then
+         logger "[OK] bluetooth device 1 [$BT1] is already connected"
+      else
+         logger "[KO] bluetooth device 1 [$BT1] connected"
+         connect $BT1 1
+
+         logger "bluetooth device 1 [$BT1] [$bt1Status]"
+         bt2Status='unknown'
+      fi
+   else
+      logger "no configuration found for bluetooth device 1 (check mac address)"
+   fi
+
+   if [ ! -z "$BT2" ]; then
+      if grep -q $BT2 /tmp/btcheck; then
+         logger "[OK] bluetooth device 2 [$BT2] is already connected"
+      else
+         logger "[KO] bluetooth device 2 [$BT2] connected"
+         connect $BT2 2
+
+         logger "bluetooth device 2 [$BT2] [$bt2Status]"
+      fi
+   else
+      logger "no configuration found for bluetooth device 2 (check mac address)"
+   fi
+
+   sleep $DELAI
+done
+
+logger "[STOP] bluetooth devices monitoring."
+#END MAIN
+
+exit 0
