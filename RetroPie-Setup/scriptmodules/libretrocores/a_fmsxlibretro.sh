418f940da86f893455c0424df5afe95456c81d81
SpikeSpiegel <frthery@gmail.com>
2014-12-23 18:27:40 +0100

    ADD a_fmsxlibretro

---
+++ RetroPie-Setup/scriptmodules/libretrocores/a_fmsxlibretro.sh
@@ -0,0 +1,35 @@
+rp_module_id="a_fmsxlibretro"
+rp_module_desc="MSX LibretroCore fmsx"
+rp_module_menus="4+"
+
+function sources_a_fmsxlibretro() {
+    gitPullOrClone "$rootdir/emulatorcores/fmsx-libretro" git://github.com/libretro/fmsx-libretro.git
+}
+
+function build_a_fmsxlibretro() {
+    pushd "$rootdir/emulatorcores/fmsx-libretro"
+    
+    [ -z "${NOCLEAN}" ] && make -f Makefile clean || echo "Failed to clean!"
+    make -f Makefile platform="${FORMAT_COMPILER_TARGET}" ${COMPILER} 2>&1 | tee makefile.log || echo -e "Failed to compile!"
+    [ -f makefile.log ] && cp makefile.log $outputdir/_log.makefile.fmsxlibretro
+
+    [ -z "$so_filter" ] && so_filter="*libretro*.so"
+    if [[ -z `find $rootdir/emulatorcores/fmsx-libretro/ -name "$so_filter"` ]]; then
+        __ERRMSGS="$__ERRMSGS Could not successfully compile MSX core."
+    fi
+
+    popd
+}
+
+function configure_a_fmsxlibretro() {
+    mkdir -p $romdir/msx
+    ensureSystemretroconfig "msx"
+    cp -a $rootdir/emulatorcores/fmsx-libretro/fMSX/ROMs/. $home/RetroPie/BIOS/
+    rps_retronet_prepareConfig
+    #setESSystem "MSX" "msx" "~/RetroPie/roms/msx" ".rom .ROM .mx1 .MX1 .mx2 .MX2 .col .COL .dsk .DSK" "$rootdir/supplementary/runcommand/runcommand.sh 4 \"$rootdir/emulators/RetroArch/installdir/bin/retroarch -L `find $rootdir/emulatorcores/fmsx-libretro/ -name \"*libretro*.so\" | head -1` --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/msx/retroarch.cfg $__tmpnetplaymode$__tmpnetplayhostip_cfile$__tmpnetplayport$__tmpnetplayframes %ROM%\"" "msx" "msx"
+}
+
+function copy_a_fmsxlibretro() {
+    [ -z "$so_filter" ] && so_filter="*libretro*.so"
+    find $rootdir/emulatorcores/fmsx-libretro/ -name $so_filter | xargs cp -t $outputdir
+}
