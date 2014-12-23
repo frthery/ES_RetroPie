300bfdc5f872a61f5dfce69adbc1d511d183e6fa
SpikeSpiegel <frthery@gmail.com>
2014-12-23 17:45:52 +0100

    ADD winbot + Fix

---
+++ RetroPie-Setup/build_winbot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pushd "/home/pi/RetroPie-Setup"
+
+#git reset --hard HEAD
+#git pull
+
+./build_retropie.sh -u
+HOST_CC=x86_64-w64-mingw32 ./build_retropie.sh -b -name=a_retroarch,a_armsneslibretro,a_fbalibretro,a_gambattelibretro,a_genesislibretro,a_gpsplibretro,a_imamelibretro,a_mednafenpcefastlibretro,a_neslibretro,a_pcsx_rearmedlibretro,a_picodrivelibretro,a_pocketsneslibretro,a_prboomlibretro,a_snes9xnextlibretro,a_stellalibretro,a_virtualjaguarlibretro,a_yabauselibretro
+
+popd
