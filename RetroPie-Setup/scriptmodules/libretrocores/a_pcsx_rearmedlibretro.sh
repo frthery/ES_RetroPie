c195be6b86e5317566e8260772188395738c45cc
SpikeSpiegel <frthery@gmail.com>
2014-12-24 08:53:04 +0100

    update pcsx

--- RetroPie-Setup/scriptmodules/libretrocores/a_pcsx_rearmedlibretro.sh
+++ RetroPie-Setup/scriptmodules/libretrocores/a_pcsx_rearmedlibretro.sh
@@ -2,6 +2,10 @@ rp_module_id="a_pcsx_rearmedlibretro"
 rp_module_desc="Playstation 1 LibretroCore pcsx_rearmed (Additional)"
 rp_module_menus="2+"
 
+function depends_a_pcsx_rearmedlibretro() {
+    rps_checkNeededPackages libpng12-dev libx11-dev
+}
+
 function sources_a_pcsx_rearmedlibretro() {
     gitPullOrClone "$rootdir/emulatorcores/pcsx_rearmed" git://github.com/libretro/pcsx_rearmed.git
 }
@@ -12,6 +16,7 @@ function build_a_pcsx_rearmedlibretro() {
     ./configure --platform=libretro
     make -f Makefile.libretro clean || echo "Failed to clean!"
     make -f Makefile.libretro ${COMPILER} USE_DYNAREC=1 BUILTIN_GPU=neon 2>&1 | tee makefile.log || echo -e "Failed to compile!"
+    #make -f Makefile.libretro ${COMPILER} 2>&1 | tee makefile.log || echo -e "Failed to compile!"
     [ -f makefile.log ] && cp makefile.log $outputdir/_log.makefile.pcsx_rearmedlibretro
 
     [ -z "$so_filter" ] && so_filter="*libretro*.so"
