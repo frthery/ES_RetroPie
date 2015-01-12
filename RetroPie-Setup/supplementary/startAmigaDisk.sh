045e31d837bb7af7015dd36e032c68a3a8ed2196
SpikeSpiegel <frthery@gmail.com>
2015-01-12 14:51:30 +0100

    Update startAmigaDisk.sh
    use uae4all_rc3_05

--- RetroPie-Setup/supplementary/startAmigaDisk.sh
+++ RetroPie-Setup/supplementary/startAmigaDisk.sh
@@ -2,13 +2,13 @@
 filename=$1
 echo "File: ["$filename"]"
 
-#pushd "/opt/retropie/emulators/uae4all_rc3_05/"
-pushd "/opt/retropie/emulators/uae4all2/"
+pushd "/opt/retropie/emulators/uae4all_rc3_05/"
+#pushd "/opt/retropie/emulators/uae4all2/"
 if [[ -f "df0.adf" ]]; then
    sudo rm df0.adf
 fi
 ln -s "$filename" df0.adf
 
-#./uae4all_cyclone -s floppy0=df0.adf
-./uae4all -s floppy0=df0.adf
+./uae4all_cyclone -s floppy0=df0.adf
+#./uae4all -s floppy0=df0.adf
 popd
