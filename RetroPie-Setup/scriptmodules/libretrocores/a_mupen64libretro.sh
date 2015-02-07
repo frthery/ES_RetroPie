891327c9b1b35eeb88170ff40517cd6551c24de2
SpikeSpiegel <frthery@gmail.com>
2015-02-07 14:52:52 +0100

    Update a_mupen64libretro.sh

--- RetroPie-Setup/scriptmodules/libretrocores/a_mupen64libretro.sh
+++ RetroPie-Setup/scriptmodules/libretrocores/a_mupen64libretro.sh
@@ -4,10 +4,11 @@ rp_module_menus="4+"
 
 function sources_a_mupen64libretro() {
     rmDirExists "$rootdir/emulatorcores/mupen64plus"
+    
     # Base repo:
     [ ${FORMAT_COMPILER_TARGET} = "win" ] && gitPullOrClone "$rootdir/emulatorcores/mupen64plus" git://github.com/libretro/mupen64plus-libretro.git
     # Freezed fixed repo:
-    #[ ${FORMAT_COMPILER_TARGET} = "armv6j-hardfloat" ] && gitPullOrClone "$rootdir/emulatorcores/mupen64plus" git://github.com/gizmo98/mupen64plus-libretro.git
+    [[ ${FORMAT_COMPILER_TARGET} =~ "armv" ]] && gitPullOrClone "$rootdir/emulatorcores/mupen64plus" git://github.com/gizmo98/mupen64plus-libretro.git
 }
 
 function build_a_mupen64libretro() {
@@ -18,11 +19,10 @@ function build_a_mupen64libretro() {
     # Add missing path --> Fix already merged https://github.com/libretro/mupen64plus-libretro/commit/c035cf1c7a2514aeb14adf51ad825208ff1a068d
     # sed -i 's|GL_LIB := -lGLESv2|GL_LIB := -L/opt/vc/lib -lGLESv2|g' Makefile
 
-    #make clean
-    #make platform=rpi 
-
-    if [ ${FORMAT_COMPILER_TARGET} = "win" ]; then
-        [ -z "${NOCLEAN}" ] && make -f Makefile clean || echo "Failed to clean!"
+    [ -z "${NOCLEAN}" ] && make -f Makefile clean || echo "Failed to clean!"
+    if [[ ${FORMAT_COMPILER_TARGET} =~ "armv" ]]; then
+        make platform=rpi 2>&1 | tee makefile.log
+    else
         make -f Makefile platform="${FORMAT_COMPILER_TARGET}" ${COMPILER} 2>&1 | tee makefile.log
     fi    
     [ ${PIPESTATUS[0]} -ne 0 ] && __ERRMSGS="Could not successfully compile N64 LibretroCore Mupen64Plus!"
