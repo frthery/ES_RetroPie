e7d26fdc11f9123a37b8397434b3db7a8b5637c1
SpikeSpiegel <frthery@gmail.com>
2015-02-07 14:45:57 +0100

    Update build_pibot.sh

--- RetroPie-Setup/build_pibot.sh
+++ RetroPie-Setup/build_pibot.sh
@@ -4,6 +4,7 @@ pushd "/home/pi/RetroPie-Setup"
 
 now=`date +%Y%m%d`
 outputdir=$(pwd)/bin/$now
+outputzip=$outputdir/cores-rpi-$now.zip
 
 #git reset --hard HEAD
 #git pull
@@ -11,6 +12,6 @@ outputdir=$(pwd)/bin/$now
 ./build_retropie.sh -u
 ./build_retropie.sh -b -name=a_retroarch,a_armsneslibretro,a_fbalibretro,a_fceunextlibretro,a_fmsxlibretro,a_gambattelibretro,a_genesislibretro,a_gpsplibretro,a_imamelibretro,a_mednafenpcefastlibretro,a_pcsx_rearmedlibretro,a_picodrivelibretro,a_pocketsneslibretro,a_prboomlibretro,a_snes9xnextlibretro,a_stellalibretro,a_virtualjaguarlibretro,a_yabauselibretro
 
-zip "$outputdir/cores-rpi-$now.zip" -j $outputdir/*.so
+zip ${outputzip} build_retropie.log -j $outputdir/*.so
 
 popd
