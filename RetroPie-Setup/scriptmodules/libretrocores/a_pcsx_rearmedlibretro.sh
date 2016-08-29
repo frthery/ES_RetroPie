339daa42017b0506dba044c252a7e38c0bae4376
SpikeSpiegel <frthery@gmail.com>
2016-08-29 15:48:21 +0200

    UPDATE PCSX MODULE

--- RetroPie-Setup/scriptmodules/libretrocores/a_pcsx_rearmedlibretro.sh
+++ RetroPie-Setup/scriptmodules/libretrocores/a_pcsx_rearmedlibretro.sh
@@ -3,7 +3,7 @@ rp_module_desc="Playstation 1 LibretroCore pcsx_rearmed (Additional)"
 rp_module_menus="2+"
 
 function depends_a_pcsx_rearmedlibretro() {
-    rps_checkNeededPackages libpng12-dev libx11-dev
+    getDepends libpng12-dev libx11-dev
 }
 
 function sources_a_pcsx_rearmedlibretro() {
@@ -14,14 +14,18 @@ function build_a_pcsx_rearmedlibretro() {
     pushd "$rootdir/emulatorcores/pcsx_rearmed"
 
     ./configure --platform=libretro
-    make clean
-    make 2>&1 | tee makefile.log
-
-    #make -f Makefile.libretro clean || echo "Failed to clean!"
-    #make -f Makefile.libretro ${COMPILER} USE_DYNAREC=1 BUILTIN_GPU=neon 2>&1 | tee makefile.log
-    #make -f Makefile.libretro ${COMPILER} USE_DYNAREC=1 2>&1 | tee makefile.log
-    [ ${PIPESTATUS[0]} -ne 0 ] && __ERRMSGS="Could not successfully compile Playstation 1 LibretroCore pcsx_rearmed!"
-    [ -f makefile.log ] && cp makefile.log $outputdir/_log.makefile.pcsx_rearmedlibretro
+    if [ $? -eq 0 ]; then
+       make clean
+       
+       make 2>&1 | tee makefile.log
+       #make -f Makefile.libretro clean || echo "Failed to clean!"
+       #make -f Makefile.libretro ${COMPILER} USE_DYNAREC=1 BUILTIN_GPU=neon 2>&1 | tee makefile.log
+       #make -f Makefile.libretro ${COMPILER} USE_DYNAREC=1 2>&1 | tee makefile.log
+       [ ${PIPESTATUS[0]} -ne 0 ] && __ERRMSGS="Could not successfully compile Playstation 1 LibretroCore pcsx_rearmed!"
+       [ -f makefile.log ] && cp makefile.log $outputdir/_log.makefile.pcsx_rearmedlibretro
+    else
+       __ERRMSGS="Could not successfully configure Playstation 1 LibretroCore pcsx_rearmed!"
+    fi
 
     popd
 }
